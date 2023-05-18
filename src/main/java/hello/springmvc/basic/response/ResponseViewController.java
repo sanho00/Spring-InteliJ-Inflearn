06ce394d26bdbbe27b1f60fb47d9b6f244e28763
김산호 <undersea9120@gmail.com>
2023-05-18 22:15:11 +0900

    김영한 Spring 강의 - 10

---
+++ src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -0,0 +1,31 @@
+package hello.springmvc.basic.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResponseViewController {
+
+    @RequestMapping("/response-view-v1")
+    public ModelAndView responseViewV1() {
+        ModelAndView mav = new ModelAndView("response/hello")
+                .addObject("data","hello!");
+
+        return mav;
+    }
+
+    @RequestMapping("/response-view-v2")
+    public String responseViewV2(Model model) {
+        model.addAttribute("data","hello!");
+
+        return "response/hello";  //뷰의 논리적 이름 반환
+    }
+
+    //너무 불명확하기 때문에 권장 하지 않음
+    @RequestMapping("/response/hello")
+    public void responseViewV3(Model model) {
+        model.addAttribute("data","hello!");
+    }
+}
